8d393f0c6fdd390edbe9ef55f993c3a26240a68f
Joe Molina <joedodaniljr123@gmail.com>
2022-02-07 10:54:39 -0400

    ejercicios 6 expReg

--- ejercicios6.js
+++ ejercicios6.js
@@ -25,29 +25,38 @@ contarLetras("ñOño porque eres tan menso ");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
+const validarNombre = (nombre = "") => {
+    if(!nombre) return console.warn("no ingresaste una cadena de texto")
+   
+    if(typeof nombre!== "string") return console.error(`el valor ${nombre} no es una cadena de texto`) 
 
+    let expReg = /^[A-Za-zÑñ\s]+$/g.test(nombre);
 
+    return(expReg)
+    ? console.info(`"${nombre}", es un nombre valido`)
+    : console.warn(`"${nombre}", NO es un nombre invalido`);
+}
 
-
-
-
+validarNombre();
+validarNombre("joe molina");
+validarNombre("joe10,35");
 
 
 
 
 //20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+
+const validarcorreo = (nombre = "") => {
+    if(!nombre) return console.warn("no ingresaste una cadena de texto")
+   
+    if(typeof nombre!== "string") return console.error(`el valor ${nombre} no es una cadena de texto`) 
+
+    let expReg = /[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test(nombre);
+
+    return(expReg)
+    ? console.info(`"${nombre}", es un email valido`)
+    : console.warn(`"${nombre}", NO es un email invalido`);
+}
+
+validarcorreo("joedodaniljr123@gmail.com")
